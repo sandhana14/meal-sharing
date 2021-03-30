fb9fd1e5334e9ce19202cbbef31aa700bb2e61f1
sandhana14 <sandhana14@gmail.com>
2021-03-30 20:53:28 +0200

    Added some Changes

--- src/client/components/Meals.js
+++ src/client/components/Meals.js
@@ -6,11 +6,17 @@ import FormMeal from "./FormMeal";
 const GetMeals = () => {
   const [meals, setMeals] = useState([]);
   const [reviews, setReviews] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [display, setDisplay] = useState(true);
 
   const fetchData = () => {
-    fetch("http://localhost:5000/api/meals/")
+    fetch(`http://localhost:5000/api/meals?title=${searchValue}`)
       .then((response) => response.json())
-      .then((meal) => setMeals(meal));
+      .then((meal) => {
+        setMeals(meal);
+        setIsLoading(false);
+      });
   };
 
   const fetchReviewsData = () => {
@@ -39,7 +45,17 @@ const GetMeals = () => {
   useEffect(() => {
     fetchData();
     fetchReviewsData();
-  }, []);
+  }, [searchValue]);
+
+  const onChangeSearchHandle = (event) => {
+    setSearchValue(event.target.value);
+    setDisplay(true);
+  };
+
+  const onClickSearchHandle = (title) => {
+    setSearchValue(title);
+    setDisplay(false);
+  };
 
   let recipeImages = [
     "https://static.toiimg.com/thumb/54308405.cms?imgsize=510571&width=800&height=800",
@@ -57,82 +73,123 @@ const GetMeals = () => {
   return (
     <>
       <div>
+        <div className="search-bar-meals">
+          <div>
+            <input
+              type="text"
+              placeholder="Search for meals..."
+              value={searchValue}
+              onChange={onChangeSearchHandle}
+            ></input>
+            <img
+              className="search-icon-meals"
+              src="https://img.icons8.com/fluent-systems-regular/24/000000/google-web-search.png"
+            />
+          </div>
+
+          <div>
+            {display &&
+              searchValue !== "" &&
+              meals.map((meal) => {
+                return (
+                  <div
+                    key={meal.id}
+                    onClick={() => {
+                      onClickSearchHandle(meal.title);
+                    }}
+                    className="meal-title-auto"
+                  >
+                    <h3>{meal.title}</h3>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+
         <div className="card-holder">
-          {meals.map((meal, index) => {
-            return (
-              <div key={meal.id} className="card">
-                <div className="image">
-                  <img
-                    src={
-                      recipeImages[index] ? recipeImages[index] : randomImage
-                    }
-                    alt="Recipe picture"
-                  ></img>
-                </div>
-                <div>
-                  <h3>{meal.title}</h3>
-                </div>
-                <hr />
-                <div>
-                  <p>{meal.price}DKK</p>
-                </div>
-                <hr />
-                <div>
-                  <p>{meal.location}</p>
-                </div>
-                <hr />
-                {reviewsAndMeals(meal.id) ? (
-                  <div className="ratings-star">
-                    <span
-                      className={
-                        reviewsAndMeals(meal.id) >= 1 ? "highlighted" : ""
+          {isLoading ? (
+            <div className="loading-text">Loading...</div>
+          ) : meals.length ? (
+            meals.map((meal, index) => {
+              return (
+                <div key={meal.id} className="card">
+                  <div className="image">
+                    <img
+                      src={
+                        recipeImages[index] ? recipeImages[index] : randomImage
                       }
-                    >
-                      ★
-                    </span>
-                    <span
-                      className={
-                        reviewsAndMeals(meal.id) >= 2 ? "highlighted" : ""
-                      }
-                    >
-                      ★
-                    </span>
-                    <span
-                      className={
-                        reviewsAndMeals(meal.id) >= 3 ? "highlighted" : ""
-                      }
-                    >
-                      ★
-                    </span>
-                    <span
-                      className={
-                        reviewsAndMeals(meal.id) >= 4 ? "highlighted" : ""
-                      }
-                    >
-                      ★
-                    </span>
-                    <span
-                      className={
-                        reviewsAndMeals(meal.id) >= 5 ? "highlighted" : ""
-                      }
-                    >
-                      ★
-                    </span>
+                      alt="Recipe picture"
+                    ></img>
                   </div>
-                ) : (
                   <div>
-                    <p>No reviews</p>
+                    <h3>{meal.title}</h3>
                   </div>
-                )}
-                <hr />
-                <div>
-                  <Link to={`/meals/${meal.id}`}>
-                    <button>More Details</button>
-                  </Link>
+                  <hr />
+                  <div>
+                    <p>{meal.price}DKK</p>
+                  </div>
+                  <hr />
+                  <div>
+                    <p>{meal.location}</p>
+                  </div>
+                  <hr />
+                  {reviewsAndMeals(meal.id) ? (
+                    <div className="ratings-star">
+                      <span
+                        className={
+                          reviewsAndMeals(meal.id) >= 1 ? "highlighted" : ""
+                        }
+                      >
+                        ★
+                      </span>
+                      <span
+                        className={
+                          reviewsAndMeals(meal.id) >= 2 ? "highlighted" : ""
+                        }
+                      >
+                        ★
+                      </span>
+                      <span
+                        className={
+                          reviewsAndMeals(meal.id) >= 3 ? "highlighted" : ""
+                        }
+                      >
+                        ★
+                      </span>
+                      <span
+                        className={
+                          reviewsAndMeals(meal.id) >= 4 ? "highlighted" : ""
+                        }
+                      >
+                        ★
+                      </span>
+                      <span
+                        className={
+                          reviewsAndMeals(meal.id) >= 5 ? "highlighted" : ""
+                        }
+                      >
+                        ★
+                      </span>
+                    </div>
+                  ) : (
+                    <div>
+                      <p>No reviews</p>
+                    </div>
+                  )}
+                  <hr />
+                  <div>
+                    <Link to={`/meals/${meal.id}`}>
+                      <button>More Details</button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          ) : (
+            <div>
+              <h2 className="no-results-meals">"No meals found"</h2>
+            </div>
+          )}
         </div>
 
         <div>
